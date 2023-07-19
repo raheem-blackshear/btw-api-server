3da765b45e9d100a9e818c80c13567da38efa569
raheem-blackshear <raheem.a.blackshear@gmail.com>
2023-07-20 00:17:51 +0800

    initial commit

---
+++ Config/awsS3Config.js
@@ -0,0 +1,12 @@
+'use strict';
+var s3BucketCredentials = {
+    "bucket": "",
+    "accessKeyId": "",
+    "secretAccessKey": "",
+    "s3URL": "",
+    "folder": ""
+};
+
+module.exports = {
+    s3BucketCredentials: s3BucketCredentials
+};
